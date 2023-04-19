b9bb9457224a73868e6dc45c2b6167b20e73daeb
Iwanthi0220 <119596786+Iwanthi0220@users.noreply.github.com>
2023-04-19 22:05:18 +0530

    test6

---
+++ app/src/main/java/com/example/fillfuel_application/Confirm_Orders.kt
@@ -0,0 +1,11 @@
+package com.example.fillfuel_application
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Confirm_Orders : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_confirm_orders)
+    }
+}
---
+++ app/src/main/java/com/example/fillfuel_application/Pending_Orders.kt
@@ -0,0 +1,11 @@
+package com.example.fillfuel_application
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Pending_Orders : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pending_orders)
+    }
+}
