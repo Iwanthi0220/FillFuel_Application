fcf5f0b40d394d2574e5adb65b612403d9cbcf6f
Iwanthi0220 <119596786+Iwanthi0220@users.noreply.github.com>
2023-04-19 21:46:53 +0530

    test5

---
+++ app/src/main/java/com/example/fillfuel_application/Fuel_Type_Quantity.kt
@@ -0,0 +1,11 @@
+package com.example.fillfuel_application
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Fuel_Type_Quantity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fuel_type_quantity)
+    }
+}
